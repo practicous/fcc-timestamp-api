e5a39a9705c37c842de9006971d27117cfd2569b
practicous <ett.oussama@gmail.com>
2016-11-24 14:27:37 +0000

    Add a solution for the challenge

--- timestamp.js
+++ timestamp.js
@@ -2,6 +2,45 @@ var express = require("express");
 
 var app = express();
 
+
+var formatDate = function(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+  var date = new Date();
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
+
+app.get('/time/:timeString', function (req, res) {
+    console.log('Req params: ', req.params);
+    
+    var dateComing = req.params.timeString;
+    
+    if(parseInt(dateComing)){
+      
+      var timestampCheck = new Date(dateComing).getTime() > 0;
+      var dateCheck = Date.parse(dateComing);
+      var natural = new Date(parseInt(dateComing));
+      
+      res.json({"unix": dateComing,  "natural": formatDate(natural)});
+      
+    }else{
+      
+      if(new Date(dateComing).getTime() > 0){
+        res.json({"unix": Date.parse(dateComing),  "natural": dateComing});
+      }
+      
+    }
+    res.json({"unix": null,  "natural": null});
+    
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
